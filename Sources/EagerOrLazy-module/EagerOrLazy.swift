1ffb7a6bfdbac2b4099cb6ddea199d7c73b5f86e
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-03-24 01:56:50 +0100

    Migrated all code from previous package (which was named simply `EagerOrLazy`).

---
+++ Sources/EagerOrLazy-module/EagerOrLazy.swift
@@ -0,0 +1,20 @@
+//
+//  EagerOrLazy.swift
+//  
+//
+//  Created by Jeremy Bannister on 3/24/23.
+//
+
+///
+@_exported import ProperValueType_module
+
+
+///
+public enum EagerOrLazy: ProperValueType {
+    
+    ///
+    case eager
+    
+    ///
+    case lazy
+}
